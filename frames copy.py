cbe1de1ede82b2e45723907b83a12cb658809654
Manigandan <manik>
2022-05-05 12:49:50 +0530

    ddd

--- frames copy.py
+++ frames copy.py
@@ -4,19 +4,23 @@ import time
 
 driver = webdriver.Chrome(executable_path="C:/Users/Manigandan/Documents/chromedriver_win32/chromedriver.exe")
 
-driver.get("https://www.selenium.dev/selenium/docs/api/java/index.html?overview-summary.html")
+driver.get("http://leafground.com/pages/frame.html")
 
-driver.switch_to.frame("packageListFrame") #first frame
-driver.find_element(By.LINK_TEXT, "org.openqa.selenium").click()
+driver.switch_to.frame(1)
+driver.find_element(By.XPATH, "//button[@id='Click']").click()
+
+time.sleep(2)
+driver.switch_to.frame(1)
+driver.find_element(By.XPATH, "//button[@id='Click1']").click()
 time.sleep(3)
 
-driver.switch_to.default_content()
+#driver.switch_to.default_content()
 
-driver.switch_to.frame("packageFrame")  #second frame
-driver.find_element(By.LINK_TEXT, "WebDriver").click()
-time.sleep(3)
+#driver.switch_to.frame("packageFrame")  #second frame
+#driver.find_element(By.LINK_TEXT, "WebDriver").click()
+#time.sleep(3)
 
-driver.switch_to.default_content()
+#driver.switch_to.default_content()
 
-driver.switch_to.frame("classFrame") # third frame
-driver.find_element(By.XPATH,"/html/body/header/nav/div[1]/div[1]/ul/li[6]/a").click()
+#driver.switch_to.frame("classFrame") # third frame
+#driver.find_element(By.XPATH,"/html/body/header/nav/div[1]/div[1]/ul/li[6]/a").click()
